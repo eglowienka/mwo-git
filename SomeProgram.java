062876c9a28374c60451194be2fcb715fcc3aa08
Ewa Glowienka <eglo@interia.pl>
2017-02-11 10:19:28 +0100

    nowa wersja z galezia satelity

--- SomeProgram.java
+++ SomeProgram.java
@@ -2,4 +2,7 @@ public class SomeProgram {
     public static void main(String[] args) {
         System.out.println("We are learning to use Git i co dalej.");
     }
+	public void manageTheSatellite() {
+    // TODO implement
+}
 }
